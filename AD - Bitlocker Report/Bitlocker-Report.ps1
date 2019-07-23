ae9623cf551a08c1107f9d63f48a4aa23ff97f4a
Dean Amiridis <fx@aselectfew.com>
2019-07-23 17:33:36 -0400

    Initial Upload - Bitlocker Report

---
+++ AD - Bitlocker Report/Bitlocker-Report.ps1
@@ -0,0 +1,18 @@
+# Important Information:
+# - Export file will export file in the same directory where the ps1 is located
+# - Export file will be saved as Bitlocker-Report_YEAR_date-HOUR-minute to avoid overwrites.
+# - Script should be ran on a domain controller that has the Bitlocker Management roles installed
+#
+# Last Modified: 07-23-2019
+
+# Required Modules
+Import-Module ActiveDirectory -ErrorAction Stop
+
+# Variables
+$Computers = get-adcomputer -filter *
+$CurrentDate = (Get-Date).ToString("yyyy_MM_dd-hh_MM")
+
+# Code Execution
+ForEach ($Computer in $Computers) {
+    Get-ADobject -searchbase $computer -filter { objectclass -eq 'msFVE-RecoveryInformation' } -properties msFVE-RecoveryPassword, whencreated | select-object @{name = "Computer Name"; Expression = { $computer.name } }, whenCreated, msFVE-RecoveryPassword | export-csv Bitlocker-Report_$CurrentDate.csv -append -NoTypeInformation
+}
---
+++ AD - Bitlocker Report/README.md
@@ -0,0 +1,19 @@
+# Bitlocker-Report
+
+Bitlocker-Report is a simple report that can provide you with all bitlocker encrypted workstations, the encrypt date, and their recovery key.
+
+## Installation
+
+Save the PS1 file to a domain controller that has the bitlocker management roles installed
+
+## Usage
+
+- Open Powershell as Administrator
+- Navigate to the directory where the PS1 was saved to
+- Execute the PS1 file
+- Open the bitlocker report CSV file that is saved within the SAME directory
+
+## Contributing
+Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
+
+Please make sure to update tests as appropriate.
