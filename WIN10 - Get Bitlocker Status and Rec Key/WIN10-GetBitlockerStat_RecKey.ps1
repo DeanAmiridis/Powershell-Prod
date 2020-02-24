8254528e190a494025b0a73684dab8877488c0d8
Dean Amiridis <fx@aselectfew.com>
2020-02-23 22:55:51 -0500

    Update WIN10-GetBitlockerStat_RecKey.ps1

--- WIN10 - Get Bitlocker Status and Rec Key/WIN10-GetBitlockerStat_RecKey.ps1
+++ WIN10 - Get Bitlocker Status and Rec Key/WIN10-GetBitlockerStat_RecKey.ps1
@@ -1,13 +1,13 @@
 ﻿$BitStatus = Get-BitLockerVolume
 $BitRecoveryKey = (Get-BitLockerVolume -MountPoint C).KeyProtector
-Write-Warning "PLEASE PUT FULL PATH WITH FILENAME AND TXT EXT!! (Ex: C:\bitlocker-report.txt)"
+Write-Warning -Message 'PLEASE PUT FULL PATH WITH FILENAME AND TXT EXT!! (Ex: C:\bitlocker-report.txt)'
 # To automate this script, comment out the SaveLOC Read-Host line below and UNcomment the line under it with the expected save location
-$SaveLOC = Read-Host "Where would you like to save the report to? (full path!)"
+$SaveLOC = Read-Host -Prompt 'Where would you like to save the report to? (full path!)'
 #SaveLoc = "C:\temp\bitlocker-report.txt"
-Write-Host "Obtaining Bitlocker Recovery Status" -BackgroundColor 'Black' -ForegroundColor 'Yellow'
+Write-Host 'Obtaining Bitlocker Recovery Status' -BackgroundColor 'Black' -ForegroundColor 'Yellow'
 $BitStatus >> $SaveLOC
-manage-bde -status >> $SaveLOC
-Write-Host "Successfully Obtained Bitlocker Recovery Status" -BackgroundColor 'Black' -ForegroundColor 'Green'
-Write-Host "Obtaining Bitlocker Recovery Keys (If applicable)" -BackgroundColor 'Black' -ForegroundColor 'Yellow'
+manage-bde.exe -status >> $SaveLOC
+Write-Host 'Successfully Obtained Bitlocker Recovery Status' -BackgroundColor 'Black' -ForegroundColor 'Green'
+Write-Host 'Obtaining Bitlocker Recovery Keys (If applicable)' -BackgroundColor 'Black' -ForegroundColor 'Yellow'
 $BitRecoveryKey >> $SaveLOC
-Write-Host "Successfully Obtained Bitlocker Recovery Keys (If applicable)" -BackgroundColor 'Black' -ForegroundColor 'Green'
+Write-Host 'Successfully Obtained Bitlocker Recovery Keys (If applicable)' -BackgroundColor 'Black' -ForegroundColor 'Green'
