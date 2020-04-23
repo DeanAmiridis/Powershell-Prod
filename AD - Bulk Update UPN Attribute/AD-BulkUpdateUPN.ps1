05e51e921a4a49b4dc016af3901955b9413869d1
Dean Amiridis <fx@aselectfew.com>
2020-04-23 10:28:33 -0400

    Update: Delimiter change

--- AD - Bulk Update UPN Attribute/AD-BulkUpdateUPN.ps1
+++ AD - Bulk Update UPN Attribute/AD-BulkUpdateUPN.ps1
@@ -4,7 +4,7 @@ import-Module ActiveDirectory
 
 # ---- Data Import ----
 Clear-Host # Start with clean powershell
-$UserAccounts = Import-Csv -Path '.\user-import.csv' -Delimiter '|'  -Header @("Name", "NewUPN")
+$UserAccounts = Import-Csv -Path '.\user-import.csv' -Delimiter ','  -Header @("Name", "NewUPN")
 Write-Host "CSV Imported Successfully" -ForegroundColor "green"
 $UserAccountsCount = $UserAccounts.Count
 Write-Host "Total Imported Accounts: $UserAccountsCount" -ForegroundColor "yellow"
