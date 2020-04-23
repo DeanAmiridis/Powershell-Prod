27fd023c0e4ea54dbe030128b88719b4ca6049d3
Dean Amiridis <fx@aselectfew.com>
2020-04-23 10:18:00 -0400

    Initial Upload: AD-BulkUpdateUPN

---
+++ AD - Bulk Update UPN Attribute/AD-BulkUpdateUPN.ps1
@@ -0,0 +1,18 @@
+#Imports active directory module
+import-Module ActiveDirectory
+
+---- Data Import ----
+Clear-Host # Start with clean powershell
+$UserAccounts = Import-Csv -Path '.\user-import.csv' -Delimiter '|'  -Header @("Name", "NewUPN")
+Write-Host "CSV Imported Successfully" -ForegroundColor "green"
+$UserAccountsCount = $UserAccounts.Count
+Write-Host "Total Imported Accounts: $UserAccountsCount" -ForegroundColor "yellow"
+write-host -nonewline "Do you want to continue? (Y/N) " -ForegroundColor "red"
+$response = read-host
+if ( $response -ne "Y" ) { exit }
+# ---- Data Import End ----
+
+# ---- Action ----
+foreach ( $UserAccount in $UserAccounts ) { 
+    Get-ADUser $Username.Name | Set-ADUser -UserPrincipalName $UserAccount.NewUPN
+} 
