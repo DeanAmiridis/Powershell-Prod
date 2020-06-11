1909c17ac9f5066d47a00c656df8a878b2b3b206
Dean Amiridis <fx@aselectfew.com>
2020-06-11 15:56:07 -0400

    Create O365-PasswordExpirationUpdate.ps1

---
+++ O365 - Password Expiration Update/O365-PasswordExpirationUpdate.ps1
@@ -0,0 +1,21 @@
+# ---- Connection to O365 ----
+Connect-MsolService # Comment out if you plan on running the script while already connected to MSOLService
+
+# ---- Variables ----
+# Default script is set to disable Password Expiration. If you would like to Enable it, set this to "False"
+$PassExpiration = $True
+
+# ---- Data Import ----
+$UPNs = Import-Csv -Path '.\UPN-import.csv' -Header @("UPN")
+Write-Host "CSV Imported Successfully" -ForegroundColor "green"
+$UPNsCount = $UPNs.Count - 1
+Write-Host "Total Imported Accounts: $UPNsCount" -ForegroundColor "yellow"
+write-host -nonewline "Do you want to continue? (Y/N) " -ForegroundColor "red"
+$response = read-host
+if ( $response -ne "Y" ) { exit }
+# ---- Data Import End ----
+
+# ---- Action ----
+foreach ( $UPN in $UPNs ) { 
+    Get-MsolUser -UserPrincipalName $UPN | Set-MsolUser -PasswordNeverExpires $PassExpiration
+}  
