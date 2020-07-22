fc28f918f697219cd149d9c913f200795a922a45
Dean Amiridis <fx@aselectfew.com>
2020-07-21 22:35:07 -0400

    Create twitch_status.ps1

---
+++ MISC - Twitch Service Status/twitch_status.ps1
@@ -0,0 +1,18 @@
+# Gets current status of twitch and reports if there are any known outages
+# To have the status page pull up when script is ran, change ie.visible to TRUE
+
+$targetUrl  = 'https://status.twitch.com/'
+$ie = New-Object -com InternetExplorer.Application 
+$ie.visible=$false
+$ie.navigate($targetUrl)
+
+while($ie.Busy) {
+     Start-Sleep -m 2000
+}
+
+$output = $ie.Document.body.innerHTML
+
+if($output -eq 'Partial Outage' -or $output -eq 'Major Outage' -or $output -eq 'Degraded Performance' )
+{Write-Host "Yes, There are outages reported on twitch." -ForegroundColor "Red"}
+else
+{Write-Host "No, There are no outages reported on twitch." -ForegroundColor "Green"}
