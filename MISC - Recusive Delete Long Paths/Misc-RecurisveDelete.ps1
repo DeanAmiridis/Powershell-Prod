23006f01556e804253da072a3c9e7d4e7025da0c
Dean Amiridis <fx@aselectfew.com>
2020-08-07 14:02:31 -0400

    Update Misc-RecurisveDelete.ps1
    Recompiled script from scratch.

--- MISC - Recusive Delete Long Paths/Misc-RecurisveDelete.ps1
+++ MISC - Recusive Delete Long Paths/Misc-RecurisveDelete.ps1
@@ -1,14 +1,10 @@
-$DirPaths = Import-Csv -Path '.\dir-path.csv'
-$EmptyDir = "C:\empty"
+# ------- Variables & CSV Import ------- 
+$DirPaths = Import-Csv -Path .\dir-path.csv -Header @("Path")
+$EmptyDir = "C:\Empty"
 Write-Host "CSV Imported Successfully" -ForegroundColor "green"
-$DirPathCount = $DirPaths.Count - 1 #If you do not have a header on your csv file, remove the " - 1" from this line.
-Write-Host "Total Imported Paths: $DirPathCount" -ForegroundColor "yellow"
-write-host -nonewline "Do you want to continue? (Y/N) " -ForegroundColor "red"
-$response = read-host
-if ( $response -ne "Y" ) { exit }
-# ---- Data Import End ----
 
-# ---- Action ----
-foreach ( $DirPath in $DirPaths ) {
-    robocopy $EmptyDir $DirPath /MIR
+# ------- Action ------- 
+foreach ( $Line in $DirPaths ) {
+    Write-Host "Deleting $Line.Path" -ForegroundColor Red
+    robocopy $EmptyDir $Line.Path /MIR
 }
