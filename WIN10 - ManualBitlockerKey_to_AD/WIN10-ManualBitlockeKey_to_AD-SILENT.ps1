c01699ef01d7f3aa01ca9f23cc6bd2e534b56b4e
Dean Amiridis <fx@aselectfew.com>
2020-09-14 17:21:47 -0400

    Bitlocker Key Backup Updates

--- WIN10 - ManualBitlockerKey_to_AD/WIN10-ManualBitlockeKey_to_AD-SILENT.ps1
+++ WIN10 - ManualBitlockerKey_to_AD/WIN10-ManualBitlockeKey_to_AD-SILENT.ps1
@@ -1,10 +1,10 @@
+# This script is completely silent to the user and can be excecuted remotely without user interruption.
+# 
+#
 $LogonServer = $Env:LOGONSERVER
 $BitRecoveryPass = (Get-BitlockerVolume).KeyProtector.KeyProtectorID
-$confirmation = Read-Host "Be sure you are connected to VPN or Corp Network! Proceed?"
-if ($confirmation -eq 'y') {
     Write-Host "KeyProtector Found:" -NoNewline -ForegroundColor "Green" ; Write-Host $BitRecoveryPass
     Write-Host "Pushing keys to active-directory server..." -ForegroundColor "Yellow"
     manage-bde -protectors -adbackup C: -id $BitRecoveryPass
     Write-Host "Pushed Recovery Key to $LogonServer." -ForegroundColor "Green"    
-}
 
--- WIN10 - ManualBitlockerKey_to_AD/WIN10-ManualBitlockeKey_to_AD.ps1
+++ WIN10 - ManualBitlockerKey_to_AD/WIN10-ManualBitlockeKey_to_AD.ps1
@@ -1,7 +1,13 @@
+# This script requires users to confirm if they are on the VPN or on corporate network prior to proceeding.
+#
+#
 $LogonServer = $Env:LOGONSERVER
 $BitRecoveryPass = (Get-BitlockerVolume).KeyProtector.KeyProtectorID
+$confirmation = Read-Host "Be sure you are connected to VPN or Corp Network! Proceed?"
+if ($confirmation -eq 'y') {
     Write-Host "KeyProtector Found:" -NoNewline -ForegroundColor "Green" ; Write-Host $BitRecoveryPass
     Write-Host "Pushing keys to active-directory server..." -ForegroundColor "Yellow"
     manage-bde -protectors -adbackup C: -id $BitRecoveryPass
     Write-Host "Pushed Recovery Key to $LogonServer." -ForegroundColor "Green"    
+}
 
