171b11f66f3c165a227f725a9f11bbf3345a4ae5
Dean Amiridis <fx@aselectfew.com>
2020-10-12 15:17:58 -0400

    Initial Upload - EXCH-Mailbox2PST

---
+++ EXCH - Mailbox To PST/EXCH-Mailbox2PST.ps1
@@ -0,0 +1,17 @@
+# ---- Error Handling ----
+# $ErrorActionPreference = "SilentlyContinue" #This is for ErrorHandling, Uncomment to ignore errors.
+
+# ---- Variables ----
+$ExportPath="C:\"
+
+# ---- Data Import ----
+$Mailboxes = Import-Csv -Path '.\mailbox-import.csv' -Delimiter ','  -Header @("mailboxID")
+Write-Host "CSV Imported Successfully" -ForegroundColor "green"
+
+# ---- Action ----
+$data = foreach ( $Mailbox in $Mailboxes ) { 
+    New-MailboxExportRequest -Mailbox $Mailbox.mailboxID -Name $mailbox.mailboxID -FilePath $ExportPath
+    Write-Host "Exported $Mailbox.MailboxID Successfully"
+    
+} 
+$data | Out-File '.\Exchange-Data-Export.csv'
