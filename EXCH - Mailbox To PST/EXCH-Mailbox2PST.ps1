5538eeee9a2b758fdfd1b8571ec9dec7ed7b6812
Dean Amiridis <fx@aselectfew.com>
2020-10-26 16:17:29 -0400

    Bug Fix: Resolved export filename issue.

--- EXCH - Mailbox To PST/EXCH-Mailbox2PST.ps1
+++ EXCH - Mailbox To PST/EXCH-Mailbox2PST.ps1
@@ -2,7 +2,8 @@
 # $ErrorActionPreference = "SilentlyContinue" #This is for ErrorHandling, Uncomment to ignore errors.
 
 # ---- Variables ----
-$ExportPath = "C:\filename.pst"
+$ExportPath = "C:\pst_exports\"
+$Export_FileType = ".pst"
 
 # ---- Data Import ----
 $Mailboxes = Import-Csv -Path '.\mailbox-import.csv' -Delimiter ','  -Header @("mailboxID")
@@ -10,7 +11,7 @@ Write-Host "CSV Imported Successfully" -ForegroundColor "green"
 
 # ---- Action ----
 $data = foreach ( $Mailbox in $Mailboxes ) { 
-    New-MailboxExportRequest -Mailbox $Mailbox.mailboxID -Name $mailbox.mailboxID -FilePath $ExportPath
+    New-MailboxExportRequest -Mailbox $Mailbox.mailboxID -Name $mailbox.mailboxID -FilePath ${ExportPath}${Mailbox.MailboxID}${Export_FileType}
     Write-Host "Exported $Mailbox.MailboxID Successfully"
     
 } 
