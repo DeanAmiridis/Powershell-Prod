44126ed22c91860e3e3f2bb1350276e83c253083
Dean Amiridis <fx@aselectfew.com>
2020-10-29 21:32:43 -0400

    Updates: Included event logging and log file exports for silent installs.

--- WIN10 - DUO Silent Installer/WIN10-DUO-Silent-Install.ps1
+++ WIN10 - DUO Silent Installer/WIN10-DUO-Silent-Install.ps1
@@ -7,6 +7,7 @@
 
 # <----- Required ** DO NOT TOUCH ** ----->
 [System.Reflection.Assembly]::LoadWithPartialName("System.Windows.Forms")
+$DUOVerification = Get-WmiObject -Class Win32_Product | where-object { $_.name -Like "Duo*" }
 
 ## <----- Variables ----->
 $APIHostname = "" # Enter your organizations DUO API Hostname here.
@@ -17,6 +18,12 @@ $TempPath = "" # This is your working directory, where the MSI will be downloade
 
 ## <----- Actions ----->
 Invoke-WebRequest $DownloadURL -OutFile $TempPath
+Write-EventLog -LogName Application -Source "DUO-Silent-Installer" -EntryType Information -EventID 3 -Message "Successfully downloaded DUO Installer to: $TempPath"
 $TempPath /S /V /qn IKEY="$IntegrationKey" SKEY="$SecretKey" HOST="$APIHostname" AUTOPUSH="#1" FAILOPEN="#1" SMARTCARD="#0" RDPONLY="#0" FAILOPEN="#1"
+Write-EventLog -LogName Application -Source "DUO-Silent-Installer" -EntryType Information -EventID 3 -Message "DUO Silently installed successfully"
 Start-Sleep -s 60 # This pauses the script for 60 seconds to allow the installer to run. You can adjust this based on your environment.
-[System.Windows.Forms.MessageBox]::Show("DUO has been installed on your machine. Please note upon next login you will be prompted to pass multifactor authentication after your password is entered.","DUO - ALERT!",1,48)
+[System.Windows.Forms.MessageBox]::Show("DUO has been installed on your machine. Please note upon next login you will be prompted to pass multifactor authentication after your password is entered.", "DUO - ALERT!", 1, 48)
+Write-EventLog -LogName Application -Source "DUO-Silent-Installer" -EntryType Information -EventID 3 -Message "Warning message provided to user."
+$DUOVerification | Out-File -Append .\DUO_Silent_Installer-Log.csv # Pulls DUO Installed version/application information and writes to log.
+Write-EventLog -LogName Application -Source "DUO-Silent-Installer" -EntryType Information -EventID 3 -Message "DUO installation confirmation completed and exported to log."
+Get-Eventlog -LogName Application -Source DUO-Silent-Installer -Newest 10 | Format-Table -Wrap | Out-File -Append .\DUO_Silent_Installer-Log.csv
