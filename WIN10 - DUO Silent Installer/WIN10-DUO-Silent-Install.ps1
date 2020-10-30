542de9ed1658ab7b8b7ade6595da2240e178acf7
Dean Amiridis <fx@aselectfew.com>
2020-10-29 21:50:58 -0400

    Update: Implemented installation verification functionality.

--- WIN10 - DUO Silent Installer/WIN10-DUO-Silent-Install.ps1
+++ WIN10 - DUO Silent Installer/WIN10-DUO-Silent-Install.ps1
@@ -22,8 +22,14 @@ Write-EventLog -LogName Application -Source "DUO-Silent-Installer" -EntryType In
 $TempPath /S /V /qn IKEY="$IntegrationKey" SKEY="$SecretKey" HOST="$APIHostname" AUTOPUSH="#1" FAILOPEN="#1" SMARTCARD="#0" RDPONLY="#0" FAILOPEN="#1"
 Write-EventLog -LogName Application -Source "DUO-Silent-Installer" -EntryType Information -EventID 3 -Message "DUO Silently installed successfully"
 Start-Sleep -s 60 # This pauses the script for 60 seconds to allow the installer to run. You can adjust this based on your environment.
-[System.Windows.Forms.MessageBox]::Show("DUO has been installed on your machine. Please note upon next login you will be prompted to pass multifactor authentication after your password is entered.", "DUO - ALERT!", 1, 48)
-Write-EventLog -LogName Application -Source "DUO-Silent-Installer" -EntryType Information -EventID 3 -Message "Warning message provided to user."
-$DUOVerification | Out-File -Append .\DUO_Silent_Installer-Log.csv # Pulls DUO Installed version/application information and writes to log.
-Write-EventLog -LogName Application -Source "DUO-Silent-Installer" -EntryType Information -EventID 3 -Message "DUO installation confirmation completed and exported to log."
-Get-Eventlog -LogName Application -Source DUO-Silent-Installer -Newest 10 | Format-Table -Wrap | Out-File -Append .\DUO_Silent_Installer-Log.csv
+If ($null -ne $DUOVerification) {
+    [System.Windows.Forms.MessageBox]::Show("DUO has been installed on your machine. Please note upon next login you will be prompted to pass multifactor authentication after your password is entered.", "DUO - ALERT!", 1, 48)
+    Write-EventLog -LogName Application -Source "DUO-Silent-Installer" -EntryType Information -EventID 3 -Message "Warning message provided to user."
+    $DUOVerification | Out-File -Append .\DUO_Silent_Installer-Log.csv # Pulls DUO Installed version/application information and writes to log.
+    Write-EventLog -LogName Application -Source "DUO-Silent-Installer" -EntryType Information -EventID 3 -Message "DUO installation confirmation completed and exported to log."
+    Get-Eventlog -LogName Application -Source DUO-Silent-Installer -Newest 10 | Format-Table -Wrap | Out-File -Append .\DUO_Silent_Installer-Log.csv
+}
+Else {
+    Write-EventLog -LogName Application -Source "DUO-Silent-Installer" -EntryType Error -EventID 3 -Message "DUO installation failed. Please rerun script!"
+    Get-Eventlog -LogName Application -Source DUO-Silent-Installer -Newest 10 | Format-Table -Wrap | Out-File -Append .\DUO_Silent_Installer-Log.csv
+}
