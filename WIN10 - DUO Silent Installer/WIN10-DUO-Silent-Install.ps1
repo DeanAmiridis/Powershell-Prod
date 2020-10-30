b1a50626faa337c5ccacbea68458a79f201cd548
Dean Amiridis <fx@aselectfew.com>
2020-10-29 22:08:01 -0400

    Update: Complete rewrite.

--- WIN10 - DUO Silent Installer/WIN10-DUO-Silent-Install.ps1
+++ WIN10 - DUO Silent Installer/WIN10-DUO-Silent-Install.ps1
@@ -18,11 +18,21 @@ $TempPath = "" # This is your working directory, where the MSI will be downloade
 
 ## <----- Actions ----->
 Invoke-WebRequest $DownloadURL -OutFile $TempPath
-Write-EventLog -LogName Application -Source "DUO-Silent-Installer" -EntryType Information -EventID 3 -Message "Successfully downloaded DUO Installer to: $TempPath"
-$TempPath /S /V /qn IKEY="$IntegrationKey" SKEY="$SecretKey" HOST="$APIHostname" AUTOPUSH="#1" FAILOPEN="#1" SMARTCARD="#0" RDPONLY="#0" FAILOPEN="#1"
-Write-EventLog -LogName Application -Source "DUO-Silent-Installer" -EntryType Information -EventID 3 -Message "DUO Silently installed successfully"
-Start-Sleep -s 60 # This pauses the script for 60 seconds to allow the installer to run. You can adjust this based on your environment.
+Start-Sleep -s 240 # This pauses the script for 60 seconds to allow the installer to run. You can adjust this based on your environment. (Ex: 240 = 4 Minutes)
+
+$FileCheck = Test-Path $TempPath
+If ($FileCheck -eq $True) {
+    Write-EventLog -LogName Application -Source "DUO-Silent-Installer" -EntryType Information -EventID 3 -Message "Successfully downloaded DUO Installer to: $TempPath"
+    $TempPath /S /V /qn IKEY="$IntegrationKey" SKEY="$SecretKey" HOST="$APIHostname" AUTOPUSH="#1" FAILOPEN="#1" SMARTCARD="#0" RDPONLY="#0" FAILOPEN="#1"
+    Start-Sleep -s 60 # This pauses the script for 60 seconds to allow the installer to run. You can adjust this based on your environment.
+}
+else {
+    Write-EventLog -LogName Application -Source "DUO-Silent-Installer" -EntryType Error -EventID 3 -Message "DUO Installation file not found during installation; Script aborted."
+    Get-Eventlog -LogName Application -Source DUO-Silent-Installer -Newest 25 | Format-Table -Wrap | Out-File -Append .\DUO_Silent_Installer-Log.csv
+    Exit
+}
 If ($null -ne $DUOVerification) {
+    Write-EventLog -LogName Application -Source "DUO-Silent-Installer" -EntryType Information -EventID 3 -Message "DUO Silently installed successfully"
     [System.Windows.Forms.MessageBox]::Show("DUO has been installed on your machine. Please note upon next login you will be prompted to pass multifactor authentication after your password is entered.", "DUO - ALERT!", 1, 48)
     Write-EventLog -LogName Application -Source "DUO-Silent-Installer" -EntryType Information -EventID 3 -Message "Warning message provided to user."
     $DUOVerification | Out-File -Append .\DUO_Silent_Installer-Log.csv # Pulls DUO Installed version/application information and writes to log.
@@ -31,5 +41,17 @@ If ($null -ne $DUOVerification) {
 }
 Else {
     Write-EventLog -LogName Application -Source "DUO-Silent-Installer" -EntryType Error -EventID 3 -Message "DUO installation failed. Please rerun script!"
-    Get-Eventlog -LogName Application -Source DUO-Silent-Installer -Newest 10 | Format-Table -Wrap | Out-File -Append .\DUO_Silent_Installer-Log.csv
 }
+## <----- Housekeeping/cleanup ----->
+$FileCheck = Test-Path $TempPath
+If ($FileCheck -eq $True) {
+    Remove-Item -Recurse -Force $TempPath
+    Write-EventLog -LogName Application -Source "DUO-Silent-Installer" -EntryType Information -EventID 3 -Message "DUO Installation file successfully purged."
+
+}
+else {
+    Write-EventLog -LogName Application -Source "DUO-Silent-Installer" -EntryType Error -EventID 3 -Message "DUO Installation file not found during cleanup."
+}
+
+## <----- Final log output ----->
+Get-Eventlog -LogName Application -Source DUO-Silent-Installer -Newest 25 | Format-Table -Wrap | Out-File -Append .\DUO_Silent_Installer-Log.csv
