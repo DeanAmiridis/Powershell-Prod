445e5cc722b456498da25fc1b2ca6b321fd4918b
Dean Amiridis <fx@aselectfew.com>
2021-02-02 09:21:35 -0500

    Create O365-MFABulkEnable.ps1

---
+++ O365 - MFA Bulk Enable/O365-MFABulkEnable.ps1
@@ -0,0 +1,24 @@
+# ---- Connection to Office365 **Uncomment to use** ----
+#Connect-MsolService
+
+# ---- Data Import Start  ----
+$Accounts = Import-Csv -Path '.\account-import.csv' -Header @("Username")
+Write-Host "CSV Imported Successfully" -ForegroundColor "green"
+$AccountsCount = $Accounts.Count - 1
+Write-Host "Total Imported Accounts: $AccountsCount" -ForegroundColor "yellow"
+write-host -nonewline "Do you want to continue? (Y/N) " -ForegroundColor "red"
+$response = read-host
+if ( $response -ne "Y" ) { exit }
+# ---- Data Import End ----
+
+# ---- Variables *DO NOT TOUCH ----
+$st = New-Object -TypeName Microsoft.Online.Administration.StrongAuthenticationRequirement
+$st.RelyingParty = "*"
+$st.State = "Enabled"
+$sta = @($st)
+
+# ---- Actions ----
+foreach ( $Account in $Accounts ) {
+    Set-MsolUser -UserPrincipalName $Account.Username -StrongAuthenticationRequirements $sta
+    write-host "Completed enabling $Account.Username..."
+}
