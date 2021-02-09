3a439495139591ecd35aa5e4c1481fe2eb343b9f
Dean Amiridis <fx@aselectfew.com>
2021-02-09 10:05:10 -0500

    Minor changes.

--- EXCH - Mailbox Permissions/EXCH-MailboxPermissions.ps1
+++ EXCH - Mailbox Permissions/EXCH-MailboxPermissions.ps1
@@ -3,10 +3,11 @@
 
 # ---- Data Import ----
 $Mailboxes = Import-Csv -Path '.\mailbox-import.csv' -Delimiter ','  -Header @("Name", "UPN")
-Write-Host "CSV Imported Successfully" -ForegroundColor "green"
+Write-Host "CSV Imported Successfully..." -ForegroundColor "green"
 
 # ---- Action ----
 $data = foreach ( $Mailbox in $Mailboxes ) { 
-    Get-MailboxPermission -Identity $Mailbox.UPN | Select-Object AccessRights,User,Identity | Format-List
+    Get-MailboxPermission -Identity $Mailbox.UPN | Select-Object AccessRights,User,Identity | Format-Table
+    Write-Host "Exporting permissions for $Mailbox.Name..." -ForegroundColor Yellow
 } 
 $data | Out-File '.\Exchange-Data-Export.csv'
