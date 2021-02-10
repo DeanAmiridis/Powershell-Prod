873dc25c8a212aa7965c37a10f4dce0c5edadb75
Dean Amiridis <fx@aselectfew.com>
2021-02-10 10:26:27 -0500

    Included step identification to Bitlocker-Report

--- AD - Bitlocker Report/Bitlocker-Report.ps1
+++ AD - Bitlocker Report/Bitlocker-Report.ps1
@@ -3,10 +3,12 @@
 # - Export file will be saved as Bitlocker-Report_YEAR_date-HOUR-minute to avoid overwrites.
 # - Script should be ran on a domain controller that has the Bitlocker Management roles installed
 #
-# Last Modified: 07-23-2019
+# Last Modified: 02-10-2021
 
 # Required Modules
 Import-Module ActiveDirectory -ErrorAction Stop
+Write-Host "Imported Active Directory Module ..." -ForegroundColor Yellow
+Write-Host "Running Bitlocker Report ..." -ForegroundColor Yellow
 
 # Variables
 $Computers = get-adcomputer -filter *
@@ -16,3 +18,6 @@ $CurrentDate = (Get-Date).ToString("yyyy_MM_dd-hh_mm")
 ForEach ($Computer in $Computers) {
     Get-ADobject -searchbase $computer -filter { objectclass -eq 'msFVE-RecoveryInformation' } -properties msFVE-RecoveryPassword, whencreated | select-object @{name = "Computer Name"; Expression = { $computer.name } }, whenCreated, msFVE-RecoveryPassword | export-csv Bitlocker-Report_$CurrentDate.csv -append -NoTypeInformation
 }
+
+# End Script/Identification
+Write-Host "Bitlocker Report Complete & Exported ..." -ForegroundColor Green
