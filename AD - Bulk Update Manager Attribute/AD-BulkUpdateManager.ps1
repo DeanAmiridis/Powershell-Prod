7a3a645b4823643bd7163c5ac5f4a1793fea5d68
Dean Amiridis <fx@aselectfew.com>
2022-01-07 16:15:33 -0500

    Initial Upload: AD-BulkUpdateManager.ps1

---
+++ AD - Bulk Update Manager Attribute/AD-BulkUpdateManager.ps1
@@ -0,0 +1,21 @@
+# ---- Module Import ----
+import-Module ActiveDirectory
+# ---- Module Import End ----
+
+# ---- Data Import ----
+Clear-Host # Start with clean powershell
+$UserAccounts = Import-Csv -Path '.\user-import.csv' -Delimiter ','  -Header @("Name", "Manager")
+Write-Host "CSV Imported Successfully" -ForegroundColor "green"
+$UserAccountsCount = $UserAccounts.Count
+Write-Host "Total Imported Accounts: $UserAccountsCount" -ForegroundColor "yellow"
+write-host -nonewline "Do you want to continue? (Y/N) " -ForegroundColor "red"
+$response = read-host
+if ( $response -ne "Y" ) { exit }
+# ---- Data Import End ----
+
+# ---- Action ----
+foreach ( $UserAccount in $UserAccounts ) {
+    Get-ADUser $UserAccount.Name | Set-ADUser -Title $UserAccount.Manager
+    Write-Host "Successfully updated Manager for $UserAccount.Name to $UserAccount.Manager"
+}
+# ---- Action End ----
