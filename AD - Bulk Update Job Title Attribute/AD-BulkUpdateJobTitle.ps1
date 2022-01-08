b67169d8f0696d375411f82c84e46495999fd7c6
Dean Amiridis <fx@aselectfew.com>
2022-01-07 19:23:11 -0500

    Bug Fix: Changed delimiter to avoid incorrect entries.

--- AD - Bulk Update Job Title Attribute/AD-BulkUpdateJobTitle.ps1
+++ AD - Bulk Update Job Title Attribute/AD-BulkUpdateJobTitle.ps1
@@ -4,7 +4,7 @@ import-Module ActiveDirectory
 
 # ---- Data Import ----
 Clear-Host # Start with clean powershell
-$UserAccounts = Import-Csv -Path '.\user-import.csv' -Delimiter ','  -Header @("Name", "JobTitle")
+$UserAccounts = Import-Csv -Path '.\user-import.csv' -Delimiter '|'  -Header @("Name", "JobTitle")
 Write-Host "CSV Imported Successfully" -ForegroundColor "green"
 $UserAccountsCount = $UserAccounts.Count
 Write-Host "Total Imported Accounts: $UserAccountsCount" -ForegroundColor "yellow"
--- AD - Bulk Update Manager Attribute/AD-BulkUpdateManager.ps1
+++ AD - Bulk Update Manager Attribute/AD-BulkUpdateManager.ps1
@@ -4,7 +4,7 @@ import-Module ActiveDirectory
 
 # ---- Data Import ----
 Clear-Host # Start with clean powershell
-$UserAccounts = Import-Csv -Path '.\user-import.csv' -Delimiter ','  -Header @("Name", "Manager")
+$UserAccounts = Import-Csv -Path '.\user-import.csv' -Delimiter '|'  -Header @("Name", "Manager")
 Write-Host "CSV Imported Successfully" -ForegroundColor "green"
 $UserAccountsCount = $UserAccounts.Count
 Write-Host "Total Imported Accounts: $UserAccountsCount" -ForegroundColor "yellow"
