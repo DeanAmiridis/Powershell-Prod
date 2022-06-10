8ecd56f5bace6eb2afdcb1e9d02944944fd7d08e
Dean Amiridis <fx@aselectfew.com>
2022-06-10 12:21:36 -0400

    Initial Upload: O365BulkCredChange

---
+++ O365 - Bulk Mailbox (cloud) Cred Change/o365-bulkcredchange.ps1
@@ -0,0 +1,18 @@
+# ---- Connection to O365 ----
+Connect-MsolService # Comment out if you plan on running the script while already connected to MSOLService
+
+# ---- Data Import ----
+$UPNs = Import-Csv -Path '.\user-import.csv' -Delimiter '|'  -Header @("Address", "NewPass")
+Write-Host "CSV Imported Successfully" -ForegroundColor "green"
+$UPNsCount = $UPNs.Count - 1
+Write-Host "Total Imported Accounts: $UPNsCount" -ForegroundColor "yellow"
+write-host -nonewline "Do you want to continue? (Y/N) " -ForegroundColor "red"
+$response = read-host
+if ( $response -ne "Y" ) { exit }
+# ---- Data Import End ----
+
+# ---- Action ----
+foreach ( $UPN in $UPNs ) { 
+    Set-MsolUserPassword -UserPrincipalName $UPN.Address -NewPassword $UPN.NewPass
+    Write-Host "Credential changed for $UPN" -ForegroundColor Green
+}  
