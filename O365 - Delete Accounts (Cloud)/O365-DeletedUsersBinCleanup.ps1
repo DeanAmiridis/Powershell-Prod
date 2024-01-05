45271562244c32fe51901f8ed9778380e5c83e51
Dean Amiridis <fx@aselectfew.com>
2024-01-05 11:08:45 -0500

    New Upload: DeletedUsersBinCleanup.ps1

---
+++ O365 - Delete Accounts (Cloud)/O365-DeletedUsersBinCleanup.ps1
@@ -0,0 +1,30 @@
+# Created by Dean Amiridis
+# Last Modified - 01/05/2024
+#
+# ---- Connection to O365 ----
+# MSOL is required for this script to run. If you do not have it installed, run the following command:
+# Install-Module MSOnline
+#
+# ---- Import of the CSV File ----
+# Example of CSV File line: Dean Amiridis|dean.amiridis@mind-shift.com
+$O365UserLists = Import-Csv -Path '.\user-import.csv'
+Write-Host "CSV Imported Successfully" -ForegroundColor "green"
+$O365UserListsCount = $O365UserLists.Count
+Write-Host "Total Imported Accounts: $O365UserListsCount" -ForegroundColor "yellow"
+write-host -nonewline "Do you want to continue? (Y/N) " -ForegroundColor "red"
+$response = read-host
+if ( $response -ne "Y" ) { exit }
+# ---- Data Import End ----
+
+# ---- Action ----
+foreach ( $O365UserList in $O365UserLists ) {
+    $Username = $O365UserList.Username
+    Remove-MsolUser -UserPrincipalName $Username -RemoveFromRecycleBin -Force
+    If ($?) {
+        Write-Host "Account $Username Deleted Successfully" -ForegroundColor "green"
+    }
+    Else {
+        Write-Host "Account $Username Failed to Delete" -ForegroundColor "red"
+    }
+
+}
