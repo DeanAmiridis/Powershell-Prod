c8fb4b21b5e80b90745ab6c71dc0851ac226ff9c
Dean Amiridis <fx@aselectfew.com>
2024-12-01 23:28:29 -0500

    Update AD-Bulk_ResetAtNextLogin.ps1

--- Active Directory/AD - Bulk Reset at Next Login/AD-Bulk_ResetAtNextLogin.ps1
+++ Active Directory/AD - Bulk Reset at Next Login/AD-Bulk_ResetAtNextLogin.ps1
@@ -1,4 +1,4 @@
-# LAST MODIFIED: 2019-09-15 13:39:54
+# LAST MODIFIED: 12/01/2024
 #
 #
 # The user-import.csv needs to be in the same path as the ps1 file.
@@ -10,10 +10,13 @@
 # ---- Data Import ----
 $ErrorActionPreference = "SilentlyContinue"
 Clear-Host # Start with clean powershell
+$logFile = "action-log.txt"
 $UserAccounts = Import-Csv -Path '.\user-import.csv'
 Write-Host "CSV Imported Successfully" -ForegroundColor "green"
+Add-Content -Path $logFile -Value "CSV Imported Successfully - $(Get-Date)"
 $UserAccountsCount = $UserAccounts.Count
 Write-Host "Total Imported Accounts: $UserAccountsCount" -ForegroundColor "yellow"
+Add-Content -Path $logFile -Value "Total Imported Accounts: $UserAccountsCount - $(Get-Date)"
 write-host -nonewline "Do you want to continue? (Y/N) " -ForegroundColor "red"
 $response = read-host
 if ( $response -ne "Y" ) { exit }
@@ -24,7 +27,14 @@ foreach ( $UserAccount in $UserAccounts ) {
     $Name = $UserAccount.Name
     $samAccountName = $UserAccount.samAccountName
     Write-Host "Target user: $Name" -ForegroundColor "Yellow"
-    Set-ADUser -Identity $samAccountName -ChangePasswordAtLogon $true
-    Write-Host "Successfully set password change at next login for: $Name" -ForegroundColor "Green"
+    try {
+        Set-ADUser -Identity $samAccountName -ChangePasswordAtLogon $true
+        Write-Host "Successfully set password change at next login for: $Name" -ForegroundColor "Green"
+        Add-Content -Path $logFile -Value "Successfully set password change at next login for: $Name - $(Get-Date)"
+    }
+    catch {
+        Write-Host "Failed to set password change at next login for: $Name" -ForegroundColor "Red"
+        Add-Content -Path $logFile -Value "Failed to set password change at next login for: $Name - $(Get-Date)"
+    }
     Write-Host " "
-} 
+}
